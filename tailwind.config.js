fb3c5faa189f2a711e7bb0c3c8882a71aa266a40
Sidney Robhez <sidney.robhez@gmail.com>
2021-05-12 21:31:10 -0500

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      itim: ['Itim'],
+      montse: ['Montserrat']
+    },
+    extend: {
+      colors: {
+        danger: '#907fa4',
+        sea: '#e1e5ea',
+        lovely: '#EEEEEE',
+        bigBlue: '#413c69'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
